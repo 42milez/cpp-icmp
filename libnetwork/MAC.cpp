a0cd86faacdbc5462a352b91f1ab03ad92880dfb
Akihiro TAKASE <42milez@gmail.com>
2018-08-14 22:24:29 +0900

    Add MAC class

---
+++ libnetwork/MAC.cpp
@@ -0,0 +1,35 @@
+#include <regex>
+#include <stdexcept>
+
+#include <boost/algorithm/string.hpp>
+
+#include "MAC.h"
+
+using std::invalid_argument;
+using std::move;
+using std::regex;
+using std::smatch;
+using std::vector;
+
+using boost::split;
+using boost::is_any_of;
+
+using namespace network;
+
+const string MAC::MAC_ADDRESS_DELIMITER = ":";
+const string MAC::MAC_ADDRESS_PATTERN = "[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}";
+
+MAC::MAC(const string mac) {
+  regex pattern(MAC_ADDRESS_PATTERN);
+  smatch match;
+
+  if (regex_match(mac, match, pattern)) {
+    split(mac_, mac, is_any_of(MAC_ADDRESS_DELIMITER));
+  } else {
+    throw invalid_argument("Invalid MAC address.");
+  }
+};
+
+MAC::MAC(string& oct1, string& oct2, string& oct3, string& oct4, string& oct5, string& oct6) {
+  mac_ = { move(oct1), move(oct2), move(oct3), move(oct4), move(oct5), move(oct6) };
+}
---
+++ libnetwork/MAC.h
@@ -0,0 +1,25 @@
+#ifndef ICMP_MAC_H
+#define ICMP_MAC_H
+
+
+#include <string>
+#include <vector>
+
+namespace network
+{
+  using std::string;
+  using std::vector;
+
+  class MAC {
+  public:
+    explicit MAC(string mac);
+    MAC(string& oct1, string& oct2, string& oct3, string& oct4, string& oct5, string& oct6);
+  private:
+    static const string MAC_ADDRESS_DELIMITER;
+    static const string MAC_ADDRESS_PATTERN;
+    vector<string> mac_;
+  };
+}
+
+
+#endif // ICMP_MAC_H
