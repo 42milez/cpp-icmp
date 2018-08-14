d1a2810fdc20ecbb1fb7015b121b9fe4aab6cfe9
Akihiro TAKASE <42milez@gmail.com>
2018-08-14 22:24:41 +0900

    Use MAC class

--- libconfig/Config.cpp
+++ libconfig/Config.cpp
@@ -1,9 +1,19 @@
+#include <arpa/inet.h>
+
+#include <boost/property_tree/ini_parser.hpp>
+
 #include "Config.h"
+
+using std::make_unique;
+
+using boost::property_tree::ptree;
 
 using namespace config;
 
 Config::Config() {
-  param_ = std::make_unique<config::Param>();
+  param_ = std::make_unique<Param>();
+  read_param("network.ini");
+  set_default_param();
 }
 
 int Config::is_target_ip_addr(InAddr &in_addr) {
@@ -14,10 +24,57 @@ int Config::is_same_subnet(InAddr &addr) {
   return 0;
 }
 
-int Config::set_default_param() {
-  return 0;
+void Config::read_param(const string& filename) {
+  ptree pt;
+  read_ini(filename, pt);
+
+  if (auto val = pt.get_optional<string>("Network.gateway")) {
+    param_->gateway.s_addr = inet_addr(val.get().c_str());
+  } else {
+    param_->gateway.s_addr = 0;
+  }
+
+  if (auto val = pt.get_optional<string>("Network.vip")) {
+    param_->vip.s_addr = inet_addr(val.get().c_str());
+  } else {
+    param_->vip.s_addr = 0;
+  }
+
+  if (auto val = pt.get_optional<string>("Network.vmask")) {
+    param_->vmask.s_addr = inet_addr(val.get().c_str());
+  } else {
+    param_->vmask.s_addr = 0;
+  }
+
+  if (auto val = pt.get_optional<string>("Network.device")) {
+    param_->device = val.get();
+  } else {
+    param_->device = "";
+  }
+
+  if (auto val = pt.get_optional<string>("Network.vmac")) {
+    param_->vmac = make_unique<MAC>(val.get());
+  }
+
+  if (auto val = pt.get_optional<int>("Network.mtu")) {
+    param_->mtu = val.get();
+  } else {
+    param_->mtu = -1;
+  }
+
+  if (auto val = pt.get_optional<int>("Network.ttl")) {
+    param_->ttl = val.get();
+  } else {
+    param_->ttl = -1;
+  }
 }
 
-int Config::read_param(string &fname) {
-  return 0;
+void Config::set_default_param() {
+  if (param_->mtu == -1) {
+    param_->mtu = DEFAULT_MTU;
+  }
+
+  if (param_->ttl == -1) {
+    param_->ttl = DEFAULT_TTL;
+  }
 }
--- libconfig/Config.h
+++ libconfig/Config.h
@@ -1,27 +1,33 @@
 #ifndef ICMP_CONFIG_H
 #define ICMP_CONFIG_H
 
+
 #include <memory>
 #include <netinet/if_ether.h>
 #include <netinet/in.h>
 #include <string>
 
+#include "libnetwork/MAC.h"
+
 namespace config
 {
-  using namespace std;
+  using std::string;
+  using std::unique_ptr;
+
+  using network::MAC;
 
   using InAddr = struct in_addr;
 
   using Param = struct {
-    string device;
-    unsigned mymac[6];
-    unsigned vmac[6];
-    InAddr myip;
+    InAddr gateway;
+    InAddr ip;
     InAddr vip;
     InAddr vmask;
-    InAddr gateway;
-    int ttl;
+    string device;
+    unique_ptr<MAC> mac;
+    unique_ptr<MAC> vmac;
     int mtu;
+    int ttl;
   };
 
   const int DEFAULT_MTU = ETHERMTU;
@@ -33,8 +39,8 @@ namespace config
     int is_target_ip_addr(InAddr &in_addr);
     int is_same_subnet(InAddr &addr);
   private:
-    int set_default_param();
-    int read_param(string &fname);
+    void read_param(const string& fname);
+    void set_default_param();
     unique_ptr<Param> param_;
   };
 }
