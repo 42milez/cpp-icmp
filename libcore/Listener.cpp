6bbd61476e54219cd1044193a11345466e052f80
Akihiro TAKASE <42milez@gmail.com>
2018-08-16 01:20:25 +0900

    Update Listener

--- libcore/Listener.cpp
+++ libcore/Listener.cpp
@@ -1,1 +1,24 @@
 #include "Listener.h"
+
+using namespace core;
+
+Listener::Listener() {
+  listener_ = make_unique<nw::NetworkIO>();
+  assign([this]{
+    listener_->wait_for_accept([this](const int fd){
+      // ...
+    });
+  });
+}
+
+Listener::~Listener() {
+  stop();
+}
+
+void Listener::start() {
+  run();
+}
+
+void Listener::stop() {
+  terminate();
+}
--- libcore/Listener.h
+++ libcore/Listener.h
@@ -2,11 +2,25 @@
 #define ICMP_LISTENER_H
 
 
+#include <memory>
+
+#include "NetworkIO.h"
 #include "Worker.h"
 
-class Listener : public Worker {
+namespace core
+{
+  namespace nw = network;
 
-};
+  class Listener : public Worker {
+  public:
+    explicit Listener();
+    ~Listener() override;
+    void start() override;
+    void stop() override;
+  private:
+    unique_ptr<nw::NetworkIO> listener_;
+  };
+} // core
 
 
 #endif // ICMP_LISTENER_H
