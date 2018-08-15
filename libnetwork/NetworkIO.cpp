ec8aa6017d19ee306da8c363fbb812710c362386
Akihiro TAKASE <42milez@gmail.com>
2018-08-16 01:32:51 +0900

    Update NetworkIO

--- libnetwork/NetworkIO.cpp
+++ libnetwork/NetworkIO.cpp
@@ -1,1 +1,31 @@
+#include <unistd.h>
+
 #include "NetworkIO.h"
+
+using namespace network;
+
+NetworkIO::NetworkIO(int port) {
+  port_ = port;
+  create_socket();
+  create_multiplexer();
+}
+
+NetworkIO::~NetworkIO() {
+  close(fd_);
+}
+
+void NetworkIO::create_socket() {
+  // ...
+}
+
+void NetworkIO::create_multiplexer() {
+  // ...
+}
+
+void NetworkIO::wait_for_accept(const function<void(const int)> &fn) {
+  // ...
+}
+
+void NetworkIO::wait_for_read(const function<void()> &fn) {
+  // ...
+}
--- libnetwork/NetworkIO.h
+++ libnetwork/NetworkIO.h
@@ -2,9 +2,26 @@
 #define ICMP_NETWORKIO_H
 
 
-class NetworkIO {
+#include <functional>
 
-};
+namespace network
+{
+  using std::function;
+
+  class NetworkIO {
+  public:
+    NetworkIO(int port);
+    ~NetworkIO();
+    void wait_for_accept(const function<void(const int fd)>& fn);
+    void wait_for_read(const function<void()>& fn);
+  private:
+    void create_socket();
+    void create_multiplexer();
+    int fd_;
+    int mux_;
+    int port_;
+  };
+}
 
 
 #endif // ICMP_NETWORKIO_H
