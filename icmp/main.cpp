b1870f16f318ad608ab3ee8ae08bdfbac97fa1cd
Akihiro TAKASE <42milez@gmail.com>
2018-08-15 12:04:58 +0900

    Tiny fixes

--- icmp/main.cpp
+++ icmp/main.cpp
@@ -33,41 +33,50 @@ namespace
 } // namespace
 
 int main(int argc, char** argv) {
-  po::options_description default_mode("DEFAULT MODE", 160);
-  auto add_default_option = default_mode.add_options();
-  add_default_option("-c,--config", po::value<string>()->value_name("<file>"),
-                     "Configure specialised network using given JSON information\n");
-  add_default_option("-h,--help", "Show this help message and exit\n");
+  po::options_description opt_desc("OPTIONS", 160);
+  opt_desc.add_options()
+    ("config,c", po::value<string>()->value_name("<file>"),
+     "Configure specialised network using given JSON information\n")
+    ("help,h", "Show this help message and exit\n");
 
   po::options_description allowed_options("Allowed options");
-  allowed_options.add(default_mode);
+  allowed_options.add(opt_desc);
 
   po::variables_map vm;
-  vector<string> unrecognisedOptions;
+  vector<string> unrecognised_options;
   try {
     po::parsed_options parsed = po::command_line_parser(argc, argv).options(allowed_options).allow_unregistered().run();
-    unrecognisedOptions = collect_unrecognized(parsed.options, po::include_positional);
+    unrecognised_options = collect_unrecognized(parsed.options, po::include_positional);
     po::store(parsed, vm);
     po::notify(vm);
-  }
-  catch (po::error const& e) {
+  } catch (po::error const &e) {
     cerr << e.what();
     return -1;
   }
 
-  for (const auto& option : unrecognisedOptions) {
+  for (const auto &option : unrecognised_options) {
     cerr << "Invalid argument: " << option << "\n";
     return -1;
   }
 
-  if (vm.count("help")) {
-    cout << "NAME:\n"
-         << "   aleth " << icmp_get_buildinfo()->project_version << '\n'
-         << "USAGE:\n"
-         << "   aleth [options]\n\n"
-         << "WALLET USAGE:\n";
-    cout << default_mode;
+  if (vm.count("h") || vm.count("help")) {
+    cout << "NAME:" << endl
+         << "   icmp " << icmp_get_buildinfo()->project_version << endl << endl
+         << "USAGE:" << endl
+         << "   icmp [options]" << endl << endl;
+    cout << opt_desc;
     return 0;
+  }
+
+  unique_ptr<config::Config> config;
+
+  if (vm.count("config")) {
+    try {
+      config = make_unique<config::Config>(vm["config"].as<string>());
+    } catch (invalid_argument const &e) {
+      cerr << "Invalid parameter: " << e.what() << endl;
+      return -1;
+    }
   }
 
   signal(SIGABRT, &ExitHandler::exitHandler);
@@ -75,14 +84,6 @@ int main(int argc, char** argv) {
   signal(SIGINT, &ExitHandler::exitHandler);
 
   ExitHandler exitHandler;
-  unique_ptr<config::Config> config;
-
-  try {
-    config = make_unique<config::Config>(argv[1]);
-  } catch (invalid_argument const& e) {
-    cerr << "Invalid parameter: " << e.what() << endl;
-    return -1;
-  }
 
   while (!exitHandler.shouldExit()) {
     // do something
