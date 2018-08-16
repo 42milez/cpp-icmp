9034692f146ea9f5a9ed511e2a19307457566974
Akihiro TAKASE <42milez@gmail.com>
2018-08-16 12:54:09 +0900

    Update Watcher

--- libcore/Watcher.cpp
+++ libcore/Watcher.cpp
@@ -1,1 +1,21 @@
 #include "Watcher.h"
+
+using namespace core;
+
+Watcher::Watcher() {
+  assign([this]{
+    // ...
+  });
+}
+
+Watcher::~Watcher() {
+  stop();
+}
+
+void Watcher::start() {
+  run();
+}
+
+void Watcher::stop() {
+  terminate();
+}
--- libcore/Watcher.h
+++ libcore/Watcher.h
@@ -1,10 +1,19 @@
-]#ifndef ICMP_WATCHER_H
+#ifndef ICMP_WATCHER_H
 #define ICMP_WATCHER_H
 
 
-class Watcher {
+#include "Worker.h"
 
-};
+namespace core
+{
+  class Watcher : Worker {
+  public:
+    explicit Watcher();
+    ~Watcher();
+    void start() override;
+    void stop() override;
+  };
+}
 
 
 #endif // ICMP_WATCHER_H
