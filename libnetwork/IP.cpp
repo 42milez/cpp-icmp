42ee4685ef5d6a9584288ffe9ea5446e5b72a585
Akihiro TAKASE <42milez@gmail.com>
2018-08-29 08:58:11 +0900

    Add IP class

---
+++ libnetwork/IP.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Akihiro TAKASE on 2018/08/28.
+//
+
+#include "IP.h"
---
+++ libnetwork/IP.h
@@ -0,0 +1,16 @@
+//
+// Created by Akihiro TAKASE on 2018/08/28.
+//
+
+#ifndef ICMP_IP_H
+#define ICMP_IP_H
+
+
+
+class IP {
+
+};
+
+
+
+#endif //ICMP_IP_H
