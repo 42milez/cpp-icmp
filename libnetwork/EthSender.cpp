c9976fe057ee2a07e5d0c320eba67895ad3e97d4
Akihiro TAKASE <42milez@gmail.com>
2018-09-02 13:28:23 +0900

    Fix undefined reference error

--- libnetwork/EthSender.cpp
+++ libnetwork/EthSender.cpp
@@ -7,9 +7,3 @@ EthSender::EthSender(const std::shared_ptr<cfg::Config> &config) {
   logger_ = spdlog::stdout_color_mt("EthSender");
   sock_ = std::make_unique<RawSocket>(config_->device());
 }
-
-template <typename T>
-void EthSender::send(u_int16_t type, const bytes &dmac, const std::unique_ptr<Payload<T>> &pl)
-{
-  // ...
-}
