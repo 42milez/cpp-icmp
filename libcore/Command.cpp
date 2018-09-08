4ab66e7840863fadf82cfd2830106930faf19210
Akihiro TAKASE <42milez@gmail.com>
2018-09-08 09:06:19 +0900

    Add Command implementation

--- libcore/Command.cpp
+++ libcore/Command.cpp
@@ -1,1 +1,29 @@
 #include "Command.h"
+
+using namespace core;
+
+Command::Command() {
+  logger_ = spdlog::stdout_color_mt("Command");
+  assign([this]{ this->wait(); });
+}
+
+Command::~Command() {
+  stop();
+}
+
+void Command::start() {
+  run();
+}
+
+void Command::stop() {
+  terminate();
+}
+
+void Command::wait() {
+#if defined(__linux__)
+  // ...
+#else
+  // Unix
+  // ...
+#endif
+}
--- libcore/Command.h
+++ libcore/Command.h
@@ -2,12 +2,22 @@
 #define ICMP_COMMAND_H
 
 
+#include <spdlog/spdlog.h>
+
+#include "libnetwork/RawSocket.h"
 #include "Worker.h"
 
 namespace core
 {
   class Command : public Worker {
-
+  public:
+    Command();
+    ~Command();
+    void start() override;
+    void stop() override;
+  private:
+    void wait();
+    std::shared_ptr<spdlog::logger> logger_;
   };
 }
 
